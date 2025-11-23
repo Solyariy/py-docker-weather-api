838d2e10ef96621babe23fd697690d6590a19ad1
solyariy <solalex005@gmai.com>
2025-11-23 21:56:53 +0200

    fix1

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.10.8-alpine
 LABEL authors="solal"
 
-ENV PYTHOUNNBUFFERED=1
+ENV PYTHONUNBUFFERED=1
 
 WORKDIR src/
 
-COPY docker-requirements.txt requirements.txt
+COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 BASE_URL = "https://api.weatherapi.com/v1"
 PARAMS_PARIS = "/current.json?key={key}&q={city}"
-
+CITY = "Paris"
 
 def get_weather() -> None:
     key = os.environ["API_KEY"]
-    url = BASE_URL + PARAMS_PARIS.format(key=key, city="Paris")
+    url = BASE_URL + PARAMS_PARIS.format(key=key, city=CITY)
     res = requests.get(url)
     print(res.json())
 
