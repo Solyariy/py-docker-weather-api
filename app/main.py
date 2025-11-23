6ad3bda5bab4090d7e707b3ae905a4fb6b682427
solyariy <solalex005@gmai.com>
2025-11-23 21:57:42 +0200

    fix3

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ BASE_URL = "https://api.weatherapi.com/v1"
 PARAMS_PARIS = "/current.json?key={key}&q={city}"
 CITY = "Paris"
 
+
 def get_weather() -> None:
     key = os.environ["API_KEY"]
     url = BASE_URL + PARAMS_PARIS.format(key=key, city=CITY)
